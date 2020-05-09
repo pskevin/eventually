3fc2cbfe883ca656a18d6dd4c64331ef9cfcf3f4
pskevin <25983646+pskevin@users.noreply.github.com>
2020-05-09 14:17:15 -0500

    detach master with cassandra integration

--- mxnet/src/kvstore/kvstore_dist_server.h
+++ mxnet/src/kvstore/kvstore_dist_server.h
@@ -39,7 +39,7 @@
 #include "../operator/tensor/elemwise_binary_op-inl.h"
 #include "../operator/tensor/init_op.h"
 
-#include "connect_cass.h"
+// #include "connect_cass.h"
 #include "cassandra.h"
 
 namespace mxnet {
@@ -100,83 +100,83 @@ class Executor {
   /**
    * \brief start the executor
    */
-  void connect_cass() {
-   CassFuture *connect_future = NULL;
-    CassCluster *cluster = cass_cluster_new();
-    cass_cluster_set_protocol_version(cluster, CASS_PROTOCOL_VERSION_V4);
-    CassSession *session = cass_session_new();
+//   void connect_cass() {
+//    CassFuture *connect_future = NULL;
+//     CassCluster *cluster = cass_cluster_new();
+//     cass_cluster_set_protocol_version(cluster, CASS_PROTOCOL_VERSION_V4);
+//     CassSession *session = cass_session_new();
     
-    char *hosts = "127.0.0.1";
+//     char *hosts = "127.0.0.1";
 
-    /* Add contact points */
-    cass_cluster_set_contact_points(cluster, hosts);
+//     /* Add contact points */
+//     cass_cluster_set_contact_points(cluster, hosts);
 
-    /* Provide the cluster object as configuration to connect the session */
-    connect_future = connect_to_cassandra(session, cluster);
+//     /* Provide the cluster object as configuration to connect the session */
+//     connect_future = connect_to_cassandra(session, cluster);
 
-    if (cass_future_error_code(connect_future) == CASS_OK)
-    {
-        CassFuture *close_future = NULL;
+//     if (cass_future_error_code(connect_future) == CASS_OK)
+//     {
+//         CassFuture *close_future = NULL;
 
-        /* Build statement and execute query */
-        const char *query = "SELECT release_version FROM system.local";
-        CassStatement *statement = cass_statement_new(query, 0);
+//         /* Build statement and execute query */
+//         const char *query = "SELECT release_version FROM system.local";
+//         CassStatement *statement = cass_statement_new(query, 0);
 
-        CassFuture *result_future = cass_session_execute(session, statement);
+//         CassFuture *result_future = cass_session_execute(session, statement);
 
-        if (cass_future_error_code(result_future) == CASS_OK)
-        {
-            /* Retrieve result set and get the first row */
-            const CassResult *result = cass_future_get_result(result_future);
-            const CassRow *row = cass_result_first_row(result);
+//         if (cass_future_error_code(result_future) == CASS_OK)
+//         {
+//             /* Retrieve result set and get the first row */
+//             const CassResult *result = cass_future_get_result(result_future);
+//             const CassRow *row = cass_result_first_row(result);
 
-            if (row)
-            {
-                const CassValue *value = cass_row_get_column_by_name(row, "release_version");
+//             if (row)
+//             {
+//                 const CassValue *value = cass_row_get_column_by_name(row, "release_version");
 
-                const char *release_version;
-                size_t release_version_length;
-                cass_value_get_string(value, &release_version, &release_version_length);
-                printf("release_version: '%.*s'\n", (int)release_version_length, release_version);
-            }
+//                 const char *release_version;
+//                 size_t release_version_length;
+//                 cass_value_get_string(value, &release_version, &release_version_length);
+//                 printf("release_version: '%.*s'\n", (int)release_version_length, release_version);
+//             }
 
-            cass_result_free(result);
-        }
-        else
-        {
-            /* Handle error */
-            const char *message;
-            size_t message_length;
-            cass_future_error_message(result_future, &message, &message_length);
-            fprintf(stderr, "Unable to run query: '%.*s'\n", (int)message_length, message);
-        }
+//             cass_result_free(result);
+//         }
+//         else
+//         {
+//             /* Handle error */
+//             const char *message;
+//             size_t message_length;
+//             cass_future_error_message(result_future, &message, &message_length);
+//             fprintf(stderr, "Unable to run query: '%.*s'\n", (int)message_length, message);
+//         }
 
-        cass_statement_free(statement);
-        cass_future_free(result_future);
+//         cass_statement_free(statement);
+//         cass_future_free(result_future);
 
-        /* Close the session */
-        close_future = cass_session_close(session);
-        cass_future_wait(close_future);
-        cass_future_free(close_future);
-    }
-    else
-    {
-        /* Handle error */
-        const char *message;
-        size_t message_length;
-        cass_future_error_message(connect_future, &message, &message_length);
-        fprintf(stderr, "Unable to connect: '%.*s'\n", (int)message_length, message);
-    }
+//         /* Close the session */
+//         close_future = cass_session_close(session);
+//         cass_future_wait(close_future);
+//         cass_future_free(close_future);
+//     }
+//     else
+//     {
+//         /* Handle error */
+//         const char *message;
+//         size_t message_length;
+//         cass_future_error_message(connect_future, &message, &message_length);
+//         fprintf(stderr, "Unable to connect: '%.*s'\n", (int)message_length, message);
+//     }
 
-    cass_future_free(connect_future);
-    cass_cluster_free(cluster);
-    cass_session_free(session);
+//     cass_future_free(connect_future);
+//     cass_cluster_free(cluster);
+//     cass_session_free(session);
 
-}
+// }
 
   void Start() {
-    printf("\n\n\nTESTING\n\n\n");
-    connect_cass();
+    // printf("\n\n\nTESTING\n\n\n");
+    // connect_cass();
     std::unique_lock<std::mutex> lk(mu_);
     while (true) {
       cond_.wait(lk, [this]{return !queue_.empty();});
